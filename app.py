fe1db3da22eb4361ddf31f1b250a52e53acf86d9
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-08-24 13:11:45 +0000

    Added an environment variable to secure mongo url

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+from os import environ
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'videogames'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config.from_envvar('MONGO_URI')
 
 mongo = PyMongo(app)
 
