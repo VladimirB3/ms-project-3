adcfa853d490a7a4c2142e92a26833b8d4a1a8d8
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-08-28 12:28:54 +0000

    Added some content t the database for testing, added a footer from Materialize - needs customization

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'videogames'
-app.config['MONGO_URI'] = 'mongodb+srv://puika:*********@myfirstcluster.lzacf.mongodb.net/**********?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://puika:*@myfirstcluster.lzacf.mongodb.net/*?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
