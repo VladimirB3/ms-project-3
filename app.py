f3d6d3769f92146a91e99d3a7d467e06c17b3f81
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-09-08 15:55:43 +0000

    Connection to the database using environment variable issues

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config["MONGO_DBNAME"] = 'videogames'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
